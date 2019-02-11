02fc5565d60869fac861ab511df631b6cd484846
Julian Rodriguez <julianrodri11@gmail.com>
2019-02-11 18:07:49 -0500

    margen en imagen

--- noticias/noticia-1.php
+++ noticias/noticia-1.php
@@ -42,7 +42,7 @@
     <div class="m-0 p-0 col-12 col-lg-6 mt-3 mt-lg-0" >
       <div class="row m-0 p-0">
         <!-- imagen -->
-        <div class="col-12">
+        <div class="col-12 p-0 m-0">
           <a class="p-0" href="<?php the_permalink() ?>">
             <?php // check if the post or page has a Featured Image assigned to it.
             if ( has_post_thumbnail() ) {
