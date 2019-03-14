e2099a6eb17a766568dcd09d71685cfd372353a1
Julian Rodriguez <julianrodri11@gmail.com>
2019-03-14 17:24:36 -0500

    Menu Listo de rectoria

--- secciones/menu_institucional.php
+++ secciones/menu_institucional.php
@@ -1,39 +1,60 @@
 <!-- menu 1 -->
-<nav class=" navbar-expand  fuente-institucional navbar-light justify-content-center bg-white texto-azul p-4 " id="navbar3" >
-  <!--div class=" "><a class=" mx-auto navbar-brand fuente-institucional texto-azul f-size-18 " data-toggle="tooltip" data-placement="auto" title="Ir al inicio" href="index"><i class="fa fa-home"></i></a></div-->
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-  <span class="navbar-toggler-icon "></span>
-  </button>
-  <ul class="nav d-flex justify-content-center  ">    
-    <li class="nav-item">
-      <a href="institucional" class="nav-link hvr-curl-top-right f-negrita fuente-institucional texto-azul text-center"  id="" role="button" aria-haspopup="true" aria-expanded="false"><img width="100%" src="<?php bloginfo('template_url')?>/img
-/institucional/informacion_institucional_aunar.png" >
-        <br><br>Información <br> Institucional <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item" id="">
-        <a href="rectoria" class=" nav-link hvr-curl-top-right f-negrita fuente-institucional texto-azul text-center "><img width="100%" src="<?php bloginfo('template_url')?>/img
-/institucional/rectoria_aunar.png" ><br><br> Rectoría</a>
-      </li>
-      <li class="nav-item" id="">
-        <a href="vicerrectorias" class=" nav-link hvr-curl-top-right f-negrita fuente-institucional texto-azul text-center"><img width="100%" src="<?php bloginfo('template_url')?>/img
-/institucional/vicerrectoria_aunar.png" ><br><br>Vicerrectorías</a>
-      </li>
-      <!--
-      <li class="nav-item" id="">
-        <a href="rectoria" class=" nav-link hvr-curl-top-right fuente-institucional texto-azul text-center"><img width="80" src="<?php bloginfo('template_url')?>/img
-/institucional/planecacion_aunar.png" ><br>Planeación</a>
-      </li>
-      <li class="nav-item" id="">
-        <a href="rectoria" class=" nav-link hvr-curl-top-right fuente-institucional texto-azul text-center"><img width="80" src="<?php bloginfo('template_url')?>/img
-/institucional/sistema_de_aseguramiento_de_calidad_aunar.png" ><br>Sistema de <br>
-          Aseguramiento <br>
-        de Calidad </a>
-      </li>
-      <li class="nav-item" id="">
-        <a href="#" class=" nav-link hvr-curl-top-right fuente-institucional texto-azul text-center"><img width="80" src="<?php bloginfo('template_url')?>/img
-/institucional/autoevaluacion_aunar.png" ><br>Autoevaluación</a>
-      </li> -->      
-    </ul>
-  </nav>
-  <!-- <center>Menú en desarrollo no terminado</center> -->
-  <!-- ------------MENÚ INSTITUCIONAL----------------------- -->
+<nav class=" navbar-expand  fuente-institucional navbar-light justify-content-center bg-white texto-azul p-4 " id="navbar3">
+	<!--div class=" "><a class=" mx-auto navbar-brand fuente-institucional texto-azul f-size-18 " data-toggle="tooltip" data-placement="auto" title="Ir al inicio" href="index"><i class="fa fa-home"></i></a></div-->
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+	 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon "></span>
+	</button>
+	<ul class="nav d-flex justify-content-center  ">
+
+		<li class="nav-item hvr-underline-from-center" id="menu_infoinstitucional">
+			<div>
+				<div class="col-12">
+					<a href="institucional" id="mn_infoinstitucional" class="nav-link hvr-curl-top-right f-negrita fuente-institucional texto-azul text-center"
+					 role="button" aria-haspopup="true" aria-expanded="false">
+						<img width="100%" src="<?php bloginfo('template_url')?>/img/iconos/ico_infoinstitucional.png">
+					</a>
+				</div>
+				<div class="col-12  d-flex mx-auto justify-content-center"">
+						<a href=" institucional" id="mn_infoinstitucional_t"
+				 class="nav-link hvr-curl-top-right f-negrita fuente-institucional texto-azul ">
+					Información <br> Institucional <span class="sr-only">(current)</span>
+					</a>
+				</div>
+			</div>
+			</a>
+		</li>
+
+		<li class="nav-item hvr-underline-from-center" id="menu_rectoria">
+			<div>
+				<div class="col-12">
+					<a href="rectoria" id="mn_rectoria" class=" nav-link hvr-curl-top-right f-negrita fuente-institucional texto-azul text-center ">
+						<img width="100%" src="<?php bloginfo('template_url')?>/img/iconos/ico_rectoria.png" alt="Rectoria AUNAR">
+					</a>
+				</div>
+				<div class="col-12 d-flex mx-auto justify-content-center">
+					<a href="rectoria" id="mn_rectoria_t" class="nav-link hvr-curl-top-right f-negrita fuente-institucional texto-azul ">
+						Rectoría
+					</a>
+				</div>
+			</div>
+		</li>
+
+		<li class="nav-item hvr-underline-from-center" id="menu_vicerrectoria">
+			<div>
+				<div class="col-12">
+					<a href="vicerrectorias" id="mn_vicerrectoria" class=" nav-link hvr-curl-top-right f-negrita fuente-institucional texto-azul text-center">
+						<img width="100%" src="<?php bloginfo('template_url')?>/img/iconos/ico_vicerrectorias.png" alt="Vicerrectorías AUNAR">
+					</a>
+				</div>
+				<div class="col-12 d-flex mx-auto justify-content-center">
+					<a href="vicerrectorias" id="mn_vicerrectoria_t" class="nav-link hvr-curl-top-right f-negrita fuente-institucional texto-azul ">Vicerrectoría
+					</a>
+				</div>
+			</div>
+		</li>
+
+	</ul>
+</nav>
+
+<!-- ------------MENÚ INSTITUCIONAL----------------------- -->
