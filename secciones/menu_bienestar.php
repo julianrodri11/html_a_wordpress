e7e3174f28a3cac000d0d1486b870d9780429d4e
Julian Rodriguez <julianrodri11@gmail.com>
2019-03-15 11:20:10 -0500

    Menú de bienestar listo

--- secciones/menu_bienestar.php
+++ secciones/menu_bienestar.php
@@ -6,35 +6,89 @@
   </button>
   <ul class="nav d-flex justify-content-center  ">
     
-    <li class="nav-item">
-      <a href="bienestar" class="nav-link hvr-curl-top-right fuente-institucional texto-azul text-center f-negrita"  id="" role="button" aria-haspopup="true" aria-expanded="false">
-        <img class="img-fluid" width="100%" src="<?php bloginfo('template_url')?>/img
-/bienestar/ico_bienestar_institucional_aunar.png" ><br>Bienestar <br> Institucional<span class="sr-only">(current)</span></a>
+    <li class="nav-item hvr-underline-from-center" id="menu_bienestar">
+      <div>
+        <div class="col-12">
+            <a href="bienestar" id="mn_bienestar" class=" nav-link hvr-curl-top-right fuente-institucional texto-azul text-center f-negrita"  id="" role="button" aria-haspopup="true" aria-expanded="false">
+            <img class="img-fluid" width="100%" src="<?php bloginfo('template_url')?>/img/iconos/ico_bienestar.png" ></a>
+        </div>
+        <div class="col-12 d-flex mx-auto justify-content-center">
+              <a href="" id="mn_bienestar_t" class="nav-link f-negrita fuente-institucional texto-azul ">
+                Bienestar <br> Institucional<span class="sr-only">(current)</span>
+              </a>
+        </div>
+      
+      </div>
       </li>
-      <li class="nav-item" id="">
-        <a href="primeros_auxilios" class=" nav-link hvr-curl-top-right fuente-institucional texto-azul text-center f-negrita ">
-        <img class="img-fluid" width="100%" src="<?php bloginfo('template_url')?>/img
-/bienestar/ico_primeros_auxilios_aunar.png" ><br> Primeros auxilios</a>
+      <li class="nav-item hvr-underline-from-center" id="menu_primeros_aux">
+        <div>
+          <div class="col-12">
+            <a href="primeros_auxilios" id="mn_primeros_aux" class=" nav-link hvr-curl-top-right fuente-institucional texto-azul text-center f-negrita ">
+            <img class="img-fluid" width="100%" src="<?php bloginfo('template_url')?>/img/iconos/ico_primeros_aux.png" ></a>
+          </div>
+          <div class="col-12 d-flex mx-auto justify-content-center">
+              <a href="" id="mn_primeros_aux_t" class="nav-link f-negrita fuente-institucional texto-azul ">
+                Primeros auxilios
+              </a>
+          </div>
+        
+      </div>
       </li>
-      <li class="nav-item" id="">
-        <a href="psicologia" class=" nav-link hvr-curl-top-right fuente-institucional texto-azul text-center f-negrita">
-        <img class="img-fluid" width="100%" src="<?php bloginfo('template_url')?>/img
-/bienestar/ico_psicologia_aunar.png" ><br>Psicología</a>
+      <li class="nav-item hvr-underline-from-center" id="menu_psicologia">
+        <div>
+          <div class="col-12">
+            <a href="psicologia" id="mn_psicologia" class=" nav-link hvr-curl-top-right fuente-institucional texto-azul text-center f-negrita">
+            <img class="img-fluid" width="100%" src="<?php bloginfo('template_url')?>/img/iconos/ico_psicologia.png" ></a>
+          </div>
+          <div class="col-12 d-flex mx-auto justify-content-center">
+              <a href="" id="mn_psicologia_t" class="nav-link f-negrita fuente-institucional texto-azul ">
+                Psicología
+              </a>
+          </div>
+        
+      </div>
       </li>
-      <li class="nav-item" id="">
-        <a href="deportes" class=" nav-link hvr-curl-top-right fuente-institucional texto-azul text-center f-negrita">
-        <img class="img-fluid" width="100%" src="<?php bloginfo('template_url')?>/img
-/bienestar/ico_deportes_aunar.png" ><br>Deportes</a>
+      <li class="nav-item hvr-underline-from-center" id="menu_deportes">
+        <div>
+          <div class="col-12">
+            <a href="deportes" id="mn_deportes" class=" nav-link hvr-curl-top-right fuente-institucional texto-azul text-center f-negrita">
+            <img class="img-fluid" width="100%" src="<?php bloginfo('template_url')?>/img/iconos/ico_deportes.png" ></a>
+          </div>
+          <div class="col-12 d-flex mx-auto justify-content-center">
+              <a href="" id="mn_deportes_t" class="nav-link f-negrita fuente-institucional texto-azul ">
+                Deportes
+              </a>
+          </div>
+        
+      </div>
       </li>
-      <li class="nav-item" id="">
-        <a href="cultura" class=" nav-link hvr-curl-top-right fuente-institucional texto-azul text-center f-negrita">
-        <img class="img-fluid" width="100%" src="<?php bloginfo('template_url')?>/img
-/bienestar/ico_cultura_aunar.png" ><br>Cultura</a>
+      <li class="nav-item hvr-underline-from-center" id="menu_cultura">
+        <div>
+          <div class="col-12">
+            <a href="cultura" id="mn_cultura" class=" nav-link hvr-curl-top-right fuente-institucional texto-azul text-center f-negrita">
+            <img class="img-fluid" width="100%" src="<?php bloginfo('template_url')?>/img/iconos/ico_cultura.png" ></a>
+          </div>
+          <div class="col-12 d-flex mx-auto justify-content-center">
+              <a href="" id="mn_cultura_t" class="nav-link f-negrita fuente-institucional texto-azul ">
+                Cultura
+              </a>
+          </div>
+        
+      </div>
       </li>
-      <li class="nav-item" id="">
-        <a href="inclusion_social" class=" nav-link hvr-curl-top-right fuente-institucional texto-azul text-center f-negrita">
-        <img class="img-fluid" width="100%" src="<?php bloginfo('template_url')?>/img
-/bienestar/ico_inclusion_social_aunar.png" ><br>Inclusión Social</a>
+      <li class="nav-item hvr-underline-from-center" id="menu_inclusion_soc">
+        <div>
+          <div class="col-12">
+            <a href="inclusion_social" id="mn_inclusion_soc" class=" nav-link hvr-curl-top-right fuente-institucional texto-azul text-center f-negrita">
+            <img class="img-fluid" width="100%" src="<?php bloginfo('template_url')?>/img/iconos/ico_inclusion_soc.png" ></a>
+          </div>
+          <div class="col-12 d-flex mx-auto justify-content-center">
+              <a href="" id="mn_inclusion_soc_t" class="nav-link f-negrita fuente-institucional texto-azul ">
+                Inclusión Social
+              </a>
+          </div>
+        
+      </div>
       </li>
       
     </ul>
