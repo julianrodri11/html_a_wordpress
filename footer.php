64ec1045c8748af5c12bc621577414e417e91e71
Julian Rodriguez <julianrodri11@gmail.com>
2019-06-19 08:58:27 -0500

    Prueba de optimización de traductor

--- footer.php
+++ footer.php
@@ -119,3 +119,9 @@
   <script  type="text/javascript" src="<?php bloginfo('template_url')?>/js/accesibilidad.js" async></script>
   <script  type="text/javascript" src="<?php bloginfo('template_url')?>/js/shepherd.js" async></script>
   <script  type="text/javascript" src="<?php bloginfo('template_url')?>/js/recorrido.js" async></script>
+  <script type="text/javascript">
+  function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'es'}, 'google_translate_element');
+  }
+  </script>
+  <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit" async></script>
--- secciones/modal-traductor.php
+++ secciones/modal-traductor.php
@@ -13,12 +13,7 @@
           <!-- -------------------------TRADUCTOR-------------------------- -->
           <!-- este script es generado automaticamente con google-->
           <div id="google_translate_element" ></div>
-          <script type="text/javascript">
-          function googleTranslateElementInit() {
-          new google.translate.TranslateElement({pageLanguage: 'es'}, 'google_translate_element');
-          }
-          </script>
-          <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit" async></script>
+          
           <!-- ---------------------FIN TRADUCTOR-------------------------- -->
         </div>
         <!-- Modal footer -->
