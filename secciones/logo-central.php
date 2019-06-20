5e8df337c9b81251b3560c13145e5f1e0b4d692f
Julian Rodriguez <julianrodri11@gmail.com>
2019-06-19 22:32:04 -0500

    cambiando imagenes a webp

--- secciones/logo-central.php
+++ secciones/logo-central.php
@@ -1,7 +1,12 @@
 <div class="d-flex justify-content-center fixed-top" >
 	<div class="col-3 col-sm-2 col-md-2 col-lg-1 p-sm-3 p-md-1  mt-md-0 mt-sm-0 mt-2 position-absolute" id="cont-logo" data-toggle="tooltip" data-placement="bottom" title="CORPORACIÓN UNIVERSITARIA    .::AUTÓNOMA DE NARIÑO::.   Ir al inicio" >
 		<div class="p-sm-2 p-md-2 p-lg-0 pt-lg-2 efecto-borde1">
-			<a href="<?php echo home_url(); ?>"> <img class="rounded-circle  img-fluid rounded mx-auto d-block position-relative  "  alt="Logo Autónoma de Nariño" src="<?php bloginfo('template_url')?>/img/logo-autonoma-de-narino.png" width="100%"></a>
+			<a href="<?php echo home_url(); ?>">
+				<picture>
+				<source class="rounded-circle  img-fluid rounded mx-auto d-block position-relative " alt="Logo Autónoma de Nariño" srcset="<?php bloginfo('template_url')?>/img/logo-autonoma-de-narino.webp" type="image/webp" " width="100%">
+				<img class="rounded-circle  img-fluid rounded mx-auto d-block position-relative " alt="Logo Autónoma de Nariño" src="<?php bloginfo('template_url')?>/img/logo-autonoma-de-narino.png" width="100%">
+				</picture>
+			</a>
 		</div>
 	</div>
 </div>
